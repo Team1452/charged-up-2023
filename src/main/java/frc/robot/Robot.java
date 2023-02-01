eb290fa2416ccedbd35a89affc1d9d6dd63d616d
Omnicats <omnicat1@windwardschool.org>
2023-01-31 17:46:46 -0800

    Add pneumatic test code (doesn't work, probably electrical issues)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -22,19 +22,21 @@ import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 
 public class Robot extends TimedRobot {
   private final XboxController controller = new XboxController(0);
-  // private final Drivetrain drive = new Drivetrain(RobotMap.MOTOR_LEFT, RobotMap.MOTOR_RIGHT);
-  private final PIDController balancer = new PIDController(0.001, 0, 0);
+
   private final CANSparkMax arm = new CANSparkMax(18, MotorType.kBrushed);
   private final DoubleSolenoid solenoid = new DoubleSolenoid(
     PneumaticsModuleType.REVPH, RobotMap.SOLENOID[0], RobotMap.SOLENOID[1]);
+  private final Compressor compressor = new Compressor(PneumaticsModuleType.CTREPCM);
 
   @Override
   public void robotInit() {
@@ -55,65 +57,28 @@ public class Robot extends TimedRobot {
 
     // System.out.println("controller: " + controller.getLeftY() + ", " + controller.getRightX() + "; speed: " + speed + "; rot:" + rot);
 
-    arm.set(controller.getLeftY());
     // drive.differentialDrive(speed, rot);
 
-    if (controller.getAButtonPressed()) {
-      solenoid.set(Value.kForward);
-    } else if (controller.getAButtonReleased()) {
-      solenoid.set(Value.kReverse);
-    }
   }
 
   @Override
   public void testInit() {
   }
 
-  PhotonCameraWrapper pcw = new PhotonCameraWrapper();
-
-  RamseteController ramsete = new RamseteController(1, 0.5);
-  PIDController turnPid = new PIDController(0.05, 0, 0);
-  PIDController distancePid = new PIDController(0.05, 0, 0);
-
   @Override
   public void testPeriodic() {
-    // Should technically be .getPitch(), but Pigeon
-    // isn't oriented/calibrated correctly
+    arm.set(controller.getLeftY());
 
-    // double pitch = drive.getGyro().getRoll();
-    // double speed = balancer.calculate(pitch, 0);
-    // System.out.println("pitch: " + pitch + "; speed: " + speed);
-    // drive.drive(speed, 0);
-  
-    var targets = pcw.getTargets(); 
-    
-    // if (!targets.isEmpty()) {
-    //   System.out.println("Detected " + targets.size() + " targets:");
-    //   for (var target : targets) {
-    //     System.out.println(target.toString());
-    //   }
-    // }
+    if (controller.getAButtonPressed()) {
+      solenoid.set(Value.kForward);
+    } else if (controller.getAButtonReleased()) {
+      solenoid.set(Value.kReverse);
+    }
 
-    Optional<EstimatedRobotPose> estimatedPose = drive.getPcw().getEstimatedGlobalPose(null);
-
-    if (estimatedPose.isPresent()) {
-      Pose2d pose = estimatedPose.get().estimatedPose.toPose2d();
-
-      Pose2d targetPose = new Pose2d(-10, 0, new Rotation2d(Units.degreesToRadians(180)));
-
-      double xErr = targetPose.getX() - pose.getX();
-      double yErr = targetPose.getY() - pose.getY();
-      
-      double target_yaw = Math.atan2(yErr, xErr);
-      double current_yaw = pose.getRotation().getRadians();
-
-      double turn = turnPid.calculate(current_yaw, target_yaw);
-      
-      double distance = Math.sqrt(xErr*xErr + yErr*yErr);
-
-      double speed = -distancePid.calculate(distance, 0);
-
-      // drive.differentialDrive(speed, turn);
+    if (controller.getBButtonPressed()) {
+      compressor.enableDigital();
+    } else if (controller.getBButtonReleased()) {
+      compressor.disable();
     }
   }
 }
